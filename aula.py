121a51fccb17f1f01e67c3be25d954585c5e4ef4
lorranyhengles <lorranyhengles@gmail.com>
2019-07-18 17:02:38 -0300

    import

---
+++ aula.py
@@ -0,0 +1,42 @@
+import numpy as np
+
+
+
+# import random
+# v_lista=[]
+# i=1
+# while i <= 20:
+#     v_lista.append(random.randrange(1,100,3))
+#     i += 1
+# print(f'Essa foi a lista gerada aleatoriamente:{v_lista}') 
+
+
+# def maior_valor(v_lista):
+#     return max(v_lista, key=int)
+
+# print(f'Esse é o maior numero: {maior_valor(v_lista)}')
+
+
+
+# lista=[]
+# lista.append(random.randrange(1,50))
+# lista.append(random.randrange(1,50))
+# lista.append(random.randrange(1,50))
+# lista.append(random.randrange(1,50))
+# lista.append(random.randrange(1,50))
+# lista.append(random.randrange(1,50))
+# lista.append(random.randrange(1,50))
+# lista.append(random.randrange(1,50))
+# lista.append(random.randrange(1,50))
+# lista.append(random.randrange(1,50))
+# lista.append(random.randrange(1,50))
+# lista.append(random.randrange(1,50))
+# lista.append(random.randrange(1,50))
+# lista.append(random.randrange(1,50))
+# lista.append(random.randrange(1,50))
+# lista.append(random.randrange(1,50))
+# lista.append(random.randrange(1,50))
+# lista.append(random.randrange(1,50))
+# print(lista)
+
+
